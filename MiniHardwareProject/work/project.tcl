6e8b8fc3b6c95dd3fb1f9a77231dc787d345e8be
benghaun <ang.benghaun@gmail.com>
2017-10-05 20:30:13 +0800

    Correct files uploaded.
    Previous files uploaded was for an older version, blame OneDrive.

--- MiniHardwareProject/work/project.tcl
+++ MiniHardwareProject/work/project.tcl
@@ -7,7 +7,7 @@ create_project $projName "$projDir/$projName" -part $device
 set_property design_mode RTL [get_filesets sources_1]
 set verilogSources [list "D:/OneDrive - Singapore University of Technology and Design/work/SUTD/50.002 Computation Structures/Mojo/MiniHardwareProject/work/verilog/mojo_top_0.v" "D:/OneDrive - Singapore University of Technology and Design/work/SUTD/50.002 Computation Structures/Mojo/MiniHardwareProject/work/verilog/reset_conditioner_1.v" "D:/OneDrive - Singapore University of Technology and Design/work/SUTD/50.002 Computation Structures/Mojo/MiniHardwareProject/work/verilog/AdderInput_2.v" "D:/OneDrive - Singapore University of Technology and Design/work/SUTD/50.002 Computation Structures/Mojo/MiniHardwareProject/work/verilog/edge_detector_3.v"]
 import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
-set ucfSources [list "D:/OneDrive\ -\ Singapore\ University\ of\ Technology\ and\ Design/work/SUTD/50.002\ Computation\ Structures/Mojo/MiniHardwareProject/constraint/custom.ucf" "C:/Program\ Files/Mojo\ IDE/library/components/io_shield.ucf" "C:/Program\ Files/Mojo\ IDE/library/components/mojo.ucf"]
+set ucfSources [list "D:/OneDrive\ -\ Singapore\ University\ of\ Technology\ and\ Design/work/SUTD/50.002\ Computation\ Structures/Mojo/MiniHardwareProject/constraint/custom.ucf" "C:/Program\ Files/Mojo\ IDE/library/components/mojo.ucf"]
 import_files -fileset [get_filesets constrs_1] -force -norecurse $ucfSources
 set_property -name {steps.bitgen.args.More Options} -value {-g Binary:Yes -g Compress} -objects [get_runs impl_1]
 set_property steps.map.args.mt on [get_runs impl_1]
